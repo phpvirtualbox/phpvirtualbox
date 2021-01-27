0613b78811c913c6854835b8fc9260e8f33fd143
Aleksandrs Aksenovskis <twingg@inbox.lv>
2021-01-27 16:20:44 +0200

    Set default cookie expiry date to 7 days after creation

--- js/utils.js
+++ js/utils.js
@@ -1120,7 +1120,7 @@ function phpVirtualBoxFailure(msg) {
  * @param {Date} expire - when cookie should expire
  */
 function vboxSetCookie(k,v,expire) {
-	var exp = (v ? (expire ? expire : new Date(2020,12,24)) : new Date().setDate(new Date().getDate() - 1));
+	var exp = (v ? (expire ? expire : new Date(Date.now() + 7 * 1000 * 60 * 60 * 24)) : new Date().setDate(new Date().getDate() - 1));
 	document.cookie = k+"="+v+"; expires="+exp.toGMTString()+"; path=/";
 	$('#vboxPane').data('vboxCookies')[k] = v;
 }
